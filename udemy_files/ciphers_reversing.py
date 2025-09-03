eb71a1f790ee32e6b0be6f783f7191a5435cf342
b1lb0ba991ns1985 <b1lb0ba991ns1985@gmail.com>
2025-09-03 12:06:19 +0100

    removing uneeded files from docker repo

--- udemy_files/ciphers_reversing.py
+++
@@ -1,9 +0,0 @@
-message = 'its no secret if two people knows it'
-translated = ''
-message_length = len(message) - 1
-
-while message_length >= 0:
-    translated += message[message_length]
-    message_length -= 1
-
-print(translated)
